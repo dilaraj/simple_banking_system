d8d6036a57a9b2e2b37bf8d9d80b331adf8484d3
dilaraj <dilara.ajaj369@gmail.com>
2026-01-15 01:04:47 +1100

    Creating a user and creating debit card info

--- bank_user.py
+++ bank_user.py
@@ -10,6 +10,7 @@ class Bank_User():
         self.email = email
         self.phone_num = phonenum
         self.password = passw
+        self.username = (fname[0:3] + lname[0:3]).lower()
 
     
     def store_user(self, new_user, userName):
@@ -20,3 +21,31 @@ class Bank_User():
                 f.write(content)
         except FileNotFoundError:
             pass
+
+    
+    def confirm_user_exists(userName):
+        user_path = Path(f'bank_users/{userName}_account.json')
+        if user_path.is_file():
+            return True
+        else:
+            return False
+        
+    
+    def load_user_file(username):
+        path = Path(f'bank_users/{username}_account.json')
+        content = path.read_text()
+        user = json.loads(content)
+
+        return user
+        
+        
+    
+    def update_user(username, new_user):
+        try:
+            path = f'bank_users/{username}_account.json'
+            content = json.dumps(new_user, indent=6)
+            with open(path, 'w') as f:
+                f.write(content)
+        except FileNotFoundError:
+            pass
+        
--- banking_system_main.py
+++ banking_system_main.py
@@ -1,4 +1,5 @@
 from bank_user import Bank_User
+import random
 
 class Banking_System:
     """Main class to run banking system"""
@@ -6,24 +7,49 @@ class Banking_System:
         pass
 
     def run_system(self):
-        print('Bank is running')
-        self._store_user_information()
+        while True:
+            option = self.display_bank_menu()
+            if option == '1':
+                self.create_new_user()
+            elif option == '2':
+                self.create_debit_account()
+            elif option == '3':
+                pass
+            elif option == '4':
+                break
+        
 
     
     def display_bank_menu(self):
-        print('Welcome to the Bank')
+        """Display bank menu"""
+        print('\nWelcome to the Bank')
         print('Please select an option using the corresponding number:')
         print('1. Create an Account')
-        print('2. Credit a debit account')
-        print('3. Quit')
+        print('2. Create a Debit Account')
+        print('3. Login to your Account')
+        print('4. Quit')
         option = input('Please enter an option: ')
 
         return option
         
     
     def create_new_user(self):
-        """gather information to create a new user"""
+        """Create a dictonary and store user object as json"""
+        new_user = self._new_user_object()        
+        user_dict = {
+            'fname': new_user.first_name,
+            'lname': new_user.last_name,
+            'uname': new_user.username,
+            'dob':new_user.DOB,
+            'email': new_user.email,
+            'phonenum': new_user.phone_num,
+            'password': new_user.password,
+            }
         
+        self._store_user_information(new_user, user_dict)
+
+    def _collect_user_information(self):
+        """Collect user information and return an array"""
         print('Create an account:')
         fname = input(f'First Name: ')
         lname = input (f'Last Name: ')
@@ -32,31 +58,57 @@ class Banking_System:
         phonenum = input(f'Phone number: ')
         password = input(f'Password: ')
 
+
+        return [fname, lname, dob, email, phonenum, password]                                                                  
+
+    def _new_user_object(self):
+        """Create a new user object"""
+        new_user = self._collect_user_information()
+        fname = new_user[0]
+        lname = new_user[1]
+        dob = new_user[2]
+        email = new_user[3]
+        phone_num = new_user[4]
+        password = new_user[5]
+
         #create a new user object
-        new_user = Bank_User(fname, lname, dob, email, phonenum, password)
-        username = (fname[0:3] + lname[0:3]).lower()
-        user_dict = {
-            'fname': fname,
-            'lname': lname,
-            'uname': username,
-            'dob':dob,
-            'email': email,
-            'phonenum': phonenum,
-            'password': password,
-            }
-
-        #return an array of needed information
-        return [new_user, user_dict, username]
+        return Bank_User(fname, lname, dob, email, phone_num, password)
     
     
-    def _store_user_information(self):
+    def _store_user_information(self, new_user, user_dict):
         """Store new user in custom file"""
-        new_user = self.create_new_user()
-
-        new_user[0].store_user(new_user[1], new_user[2])
-        print(f'Account successfully created!\nYour username is: {new_user[2]}\nPlease use this username when signing in')
+        new_user.store_user(user_dict, new_user.username)
+        print(f'Account successfully created!\nYour username is: {new_user.username}\nPlease use this username when signing in')
                                 
 
+    def create_debit_account(self):
+        username = input('Enter username: ')
+        user_exists = Bank_User.confirm_user_exists(username)
+        if user_exists:
+            #confirm password
+            user = Bank_User.load_user_file(username)
+            entered_password = input('Enter Password: ')
+
+            if entered_password == user['password']:
+                print('Welcome to Debit Card Creation Centre')
+                #create debit account
+                self._create_debit_card_info(user, username)
+            else:
+                print('Password Incorrect!')
+        else: 
+            print('Username does not exist')
+
+
+    def _create_debit_card_info(self, user, username):
+        debit_number = random.randint(1000000000000000, 9999999999999999)
+        debit_pin = input('Enter 4 digit pin: ')
+
+        debit_info = {'debit_num':debit_number, 'debit_pin':debit_pin}
+        user.update({'debit_info':debit_info})
+        Bank_User.update_user(username, user)
+        print('Card was created successfully')
+
+         
 
 #running banking system
 bs = Banking_System()
